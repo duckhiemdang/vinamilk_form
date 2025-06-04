ea5f4bcea6d6442fd8c50abf1a6bf1e99e2576eb
duckhiem2003 <duckhiem2003@gmail.com>
2025-06-04 17:17:07 +0700

    update admin responsive design

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2510689484"
+    serviceWorkerVersion: "1558858540"
   }
 });
