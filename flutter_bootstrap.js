389d82a0c0f2940ecc338695feb731ed35933d9c
duckhiem2003 <duckhiem2003@gmail.com>
2025-06-13 16:32:44 +0700

    fix admin details

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "782900310"
+    serviceWorkerVersion: "3396462741"
   }
 });
